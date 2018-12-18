4fe91d18bd530d076a181c175849002825150475
mari756h <mari756h@gmail.com>
2018-12-18 15:01:42 +0100

    change script structure

--- CBOW_no_order_proteins.py
+++ CBOW_no_order_proteins.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 
 # Import packages
-seed = 42
-import numpy as np
-#np.random.seed(seed)
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
 import torch.utils.data as data_utils
-#torch.manual_seed(seed)
 
 import argparse
 from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
+import numpy as np
 import sys
 import os
+from functions import *
 
-# To print running 
-#from tqdm import tqdm
 
 #########################
 #       Parser
@@ -35,174 +31,13 @@ def get_args():
     parser.add_argument('-embed', required=False, dest='embedding_dim', type=int, help='Number of embedding dimensions.')
     parser.add_argument('-r', required=False, dest='resume', type=str, help='Filename for saved checkpoint.')
     parser.add_argument('-test', required=False, dest='test', action='store_true', help='If this is to test model.')
+    parser.add_argument('-tsne', required=False, dest='tsne', action='store_true', help='If the test makes t-SNE plot.')
+
     
     # Set defaults
     parser.set_defaults(window_direction='both', padding=True, window_size=3, batch_size=128, epochs=10, resume=False, embedding_dim=100, test=False)
     return parser
 
-
-#########################
-# # Data loading
-#########################
-# Data loader class
-class DataLoader: 
-    def __init__(self):
-        self.corpus = []
-    
-    # Load words
-    def load_corpus(self, path): 
-        print('# Loading corpus...')
-        with open(path, 'r') as infile: 
-            for line in infile: 
-                line = line[:-1].split()
-                self.corpus.append(line)
-        print('\tDone\n')
-    
-    # Make dict
-    def count_corpus(self, padding=True, verbose=False): 
-        print('# Building vocabulary...')
-        # Count occurrences
-        unique, counts = np.unique(np.array([item for sublist in self.corpus for item in sublist]), return_counts=True)
-        self.corpus_counts = dict(zip(unique, counts))
-
-        if verbose: 
-            for v, k in sorted(zip(counts, unique), reverse=True): 
-                print('Key is "{0}" with count {1}'.format(k, v))
-
-        # Build vocabulary
-        if padding: 
-            indices = list(range(1,len(unique)+1))
-            self.word_to_idx = dict(zip(sorted(unique), indices))
-            self.word_to_idx['padding'] = 0
-        else: 
-            indices = list(range(len(unique)))
-            self.word_to_idx = dict(zip(sorted(unique), indices))
-        
-        # Make reverse dict
-        self.idx_to_word = {w: idx for idx, w in self.word_to_idx.items()}
-        
-        print('\tDone\n')
-            
-    # Function to make context pairs
-    def make_context_pairs(self, window_size=2, padding=True, direction='both'): 
-        print('# Making context pairs...') 
-        self.window_size = window_size
-        self.direction = direction
-        
-        # Run through each sample
-        self.word_data = []
-        window_slide = {'both': [-window_size, window_size], 'before': [-window_size, 0], 'after': [0, window_size]}
-        ranges = {'both': [window_size, window_size], 'before': [window_size, 0], 'after': [0, window_size]}
-        for line in self.corpus: 
-            if padding: 
-                # Add padding corresponding to the size of the window on either side
-                padding = ['padding']*window_size
-
-                # Set direction of padding
-                if self.direction=='both': 
-                    line = padding+line+padding
-                elif self.direction=='before': 
-                    line = padding+line
-                elif self.direction=='after': 
-                    line = line+padding
-                else: 
-                    print('Specify window direction!')
-                    sys.exit(1)
-
-                # Window ranges
-                start, end = ranges[self.direction]
-            else: 
-                start = 0
-                end = 0
-
-            # Make contexts
-            for i in range(start, len(line)-end):
-                c, end_slide = window_slide[self.direction]
-
-                # Run through window
-                context = []
-                while c <= end_slide:
-                    if c != 0: 
-                        context.append(line[i+c])
-                    c += 1
-                self.word_data.append((context, line[i]))
-        print('\tDone\n')
-
-    # Convert word_data to numpy array tuples
-    def words_to_index(self, word2idx):
-        if hasattr(self, 'window_size') and hasattr(self, 'direction'): 
-            print('# Converting words to indices...')
-            
-            # Pre-allocate
-            if self.direction == 'both': 
-                columns = self.window_size*2
-            else: 
-                columns = self.window_size
-
-            data = np.empty((len(self.word_data), columns), dtype=int)
-            labels = np.empty((len(self.word_data)), dtype=int)
-            print(data.shape)
-            
-            # Run through context pairs and fill arrays
-            i = 0
-            for d, l in self.word_data: 
-                data[i, :] = np.array([word2idx[w] for w in d])
-                labels[i,] = word2idx[l]
- 
-                i += 1
-                
-            # Save as tuple
-            self.context_array = (data, labels)
-            
-            # Remove word_data
-            del self.word_data
-            
-            print('\tDone\n')
-        else: 
-            print('# Make context pairs, first!')
-            sys.exit(1)
-
-####################
-# # CBOW class
-####################
-class cbow(nn.Module):
-
-    def __init__(self, vocab_size, embedding_dim=20, padding=True):
-        super(cbow, self).__init__()
-        # num_embeddings is the number of words in your train, val and test set
-        # embedding_dim is the dimension of the word vectors you are using
-        if padding: 
-            self.embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim, 
-                                          padding_idx=0)
-        else: 
-            self.embeddings = nn.Embedding(num_embeddings=vocab_size, embedding_dim=embedding_dim, 
-                                          padding_idx=None)
-        
-        self.linear_out = nn.Linear(in_features=embedding_dim, out_features=vocab_size, bias=False)
-
-    def forward(self, inputs):
-        embeds = self.embeddings(inputs)
-        
-        # To not care about the order of the words we take the mean of the time dimension
-        means = torch.mean(embeds, dim=1)
-        
-        # Softmax on output
-        #probs = F.log_softmax(out, dim=1)
-        probs = F.log_softmax(self.linear_out(means), dim=1)
-        
-        return probs
-
-# Estimate performance
-def accuracy(y_true, y_pred):
-    # Make y_pred for the word with max probability
-    values, indices = torch.max(input=y_pred, dim=1)
-    
-    # Check if indices match
-    check = torch.eq(indices, y_true)
-    
-    # Estimate accuracy
-    acc = check.sum().item()/len(check)
-    return acc
 
 ########################################################################
 #       MAIN FUNCTION
@@ -235,6 +70,9 @@ def main(args):
 
     # Convert to numpy
     train_data.words_to_index(word2idx=train_data.word_to_idx)
+
+    # Save word2idx
+    torch.save(train_data.word_to_idx, 'word2idx.pt')
 
 
     ############################
@@ -278,7 +116,7 @@ def main(args):
     net = cbow(vocab_size=len(train_data.word_to_idx), embedding_dim=args.embedding_dim, padding=pad)
     #optimizer = optim.SGD(net.parameters(), lr=0.01)
     #optimizer = optim.Adam(net.parameters(), lr=0.01)
-    optimizer = optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-5)
+    optimizer = optim.SGD(net.parameters(), lr=0.0001, momentum=0.9, weight_decay=1e-5)
     #optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.5)
 
     # If GPU is available
@@ -377,7 +215,7 @@ def main(args):
         log_file.write(str(epoch+1)+'\ttest\t'+str(test_losses/test_lengths)+'\t'+str(np.exp(test_losses/test_lengths))+'\t'+str(test_accs/test_lengths)+'\n')
 
         # Show top N validation samples and their results
-        print('# Predition examples: prediction | target | input')
+        print('# Prediction examples: prediction | target | input')
         for items in examples: 
             i, l, p = items
 
@@ -386,8 +224,23 @@ def main(args):
             pred2word = train_data.idx_to_word[p]
             targ2word = train_data.idx_to_word[l]
             print('\t', pred2word, '|', targ2word, '|', input2word)
-            print(pred2word + ' | ' + input2word + ' | ' + str(input2word))
+            #print(pred2word + ' | ' + input2word + ' | ' + str(input2word))
         print('\n')
+
+        log_file.close()
+
+        ### t-SNE plot
+        if args.tsne:  # Made by Hannah Martiny
+            # get words / amino acids that are unique
+            words = sorted(train_data.corpus_counts.wc, key=train_data.corpus_counts.wc.get, reverse=True)
+            words_array = np.array(words)
+
+            # get learned embeddings
+            idx2vec = net.embeddings.weight.data.cpu().numpy()
+
+            plot_name = 'results/' + args.post_fix + '_tSNE.png'
+            plot_tSNE(idx2vec=idx2vec, word2idx=train_data.word_to_idx, words=words_array, filename=plot_name, use_cuda=use_cuda)
+
 
 
     #########################
